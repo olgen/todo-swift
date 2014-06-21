84b3c3deca06ebf1929232bc0fc497aba5839f87
Eugen Martin <eugeniusmartinus@gmail.com>
2014-06-21 18:29:11 +0200

    save tasks to db using CoreData - add CoreData - add Model.xdatamodeld - add loading/saving functionality to TaskManager - implement missing functions for CoreData in AppDelegate - rename task -> Task

--- todo/TaskManager.swift
+++ todo/TaskManager.swift
@@ -1,17 +1,57 @@
 import UIKit
+import CoreData
 
 var taskMgr = TaskManager()
 
-struct task {
-    var name = "Unnamed"
-    var desc = "Undescribed"
+struct Task {
+    var name = ""
+    var desc = ""
 }
 
 class TaskManager: NSObject {
-    var tasks = task[]()
+    var tasks = Task[]()
     
     func addTask(name: String, desc: String){
-       tasks.append(task(name: name, desc: desc))
+        let task = Task(name: name, desc: desc)
+        tasks.append(task)
+        saveTask(task)
     }
-
+    
+    func saveTask(task: Task) {
+        var context = ctx()
+        var dbTask = NSEntityDescription.insertNewObjectForEntityForName("Tasks", inManagedObjectContext: context) as NSManagedObject
+        dbTask.setValue(task.name, forKey: "name")
+        dbTask.setValue(task.desc, forKey: "desc")
+        context.save(nil)
+        
+        println(dbTask)
+        println("Task saved!")
+    }
+    
+    func loadTasks(){
+        var context = ctx()
+        var request = NSFetchRequest(entityName: "Tasks")
+        request.returnsObjectsAsFaults = false
+        
+        // request.predicate = NSPredicate(format: "username = %@", txtUsername.text as String)
+        
+        var results:NSArray = context.executeFetchRequest(request, error: nil)
+        if (results.count > 0){
+            
+            for res in results {
+               var task = Task(name: res.valueForKey("name") as String,
+                desc: res.valueForKey("desc") as String)
+                tasks.append(task)
+            }
+        } else {
+            println("ERROR: Zero results returned!")
+        }
+        
+    }
+    
+    func ctx()-> NSManagedObjectContext {
+        var appDel = UIApplication.sharedApplication().delegate as AppDelegate
+        return appDel.managedObjectContext
+    }
+    
 }
